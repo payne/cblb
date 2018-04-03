46049891e69eeabd5a48189398ec960ebe1f1416
Matt Payne <Payne@MattPayne.org>
2018-04-02 20:21:37 -0500

    API key etc added.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+       apiKey: 'AIzaSyD11jJJivOFgudz_SI_0_0y0aDaFIhAcCE',
+       authDomain: 'cblb-91230.firebaseapp.com',
+       databaseURL: 'https://cblb-91230.firebaseio.com',
+       projectId: 'cblb-91230',
+       storageBucket: 'cblb-91230.appspot.com',
+       messagingSenderId: '591733565886'
     }
 };
